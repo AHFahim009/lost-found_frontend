43555ad5edca11f92f86fa998868247049550db6
AHFahim009 <ahfba009@gmail.com>
2024-06-12 21:15:42 +0600

    fixed

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,15 +1,14 @@
 // Need to use the React-specific entry point to import createApi
-import { axiosBaseQuery } from '@/axios/axiosBaseApi'
-import { createApi } from '@reduxjs/toolkit/query/react'
-import { tagProviders } from './tagTypes'
-
-
-
+import { axiosBaseQuery } from "@/axios/axiosBaseApi";
+import { createApi } from "@reduxjs/toolkit/query/react";
+import { tagProviders } from "./tagTypes";
 
 export const baseApi = createApi({
-  reducerPath: 'baseApi',
-  baseQuery: axiosBaseQuery({ baseUrl: "http://localhost:8000/api" }),
+  reducerPath: "baseApi",
+  baseQuery: axiosBaseQuery({
+    baseUrl:
+      "https://nextassing09server2024-ahfahim009s-projects.vercel.app/api",
+  }),
   endpoints: () => ({}),
-  tagTypes: tagProviders
-})
-
+  tagTypes: tagProviders,
+});
